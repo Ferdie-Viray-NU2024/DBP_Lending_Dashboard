8456fc966ed94d6258f10b134dfc3c5ccbd0868a
Ferdie-Viray-NU2024 <168709589+Ferdie-Viray-NU2024@users.noreply.github.com>
2025-08-08 19:33:43 +0800

    Update Dashboard_2.py

--- Dashboard_2.py
+++ Dashboard_2.py
@@ -31,24 +31,22 @@ else:
 df_yearend = pd.read_csv("DATASET_PYTHON CLEANED_YEAREND.csv", encoding='latin1', header=0)
 
 df["MONTH_YEAR"] = df["Month Name"].astype(str) + "-" + df["YEAR_ID"].astype(str)
+df.columns = df.columns.str.strip()
 
+##############################################################################
 #col1, col2 = st.columns((2))
-
 #df["CUTOFF_DATE"] = pd.to_datetime(df["CUTOFF_DATE"])
 #df_yearend["CUTOFF_DATE"] = pd.to_datetime(df["CUTOFF_DATE"])
-
 #Getting the min and max year
 #start_year = pd.to_datetime(df["CUTOFF_DATE"]).min()
 #end_year = pd.to_datetime(df["CUTOFF_DATE"]).max()
-
 #with col1:
 #    date1 = pd.to_datetime(st.date_input("Start Year: ", start_year))
-
 #with col2:
 #    date2 = pd.to_datetime(st.date_input("End Year: ", end_year))
-
 #df = df[(df["CUTOFF_DATE"] >= date1) & (df["CUTOFF_DATE"] <= date2)].copy()
 #df_yearend = df_yearend[(df["CUTOFF_DATE"] >= date1) & (df_yearend["CUTOFF_DATE"] <= date2)].copy()
+##############################################################################
 
 st.sidebar.header("Select a Filter: ")
 
@@ -1403,6 +1401,7 @@ with cl22:
             mime="text/csv",
             help="Click here to download the CSV file"
         )
+
 
 
 
